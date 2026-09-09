f83a64dfd0ccdd610e347f39daf1adb1bc249bcb
Aryan sahu <lakhan9953@gmail.com>
2026-09-09 21:25:42 +0530

    Learning function declaration and Function calling in java

---
+++ funtion.java
@@ -0,0 +1,17 @@
+package JAVA_ARRAYS;
+
+public class funtion {
+    public static int solve(){
+        System.out.println("Hello World");
+        int a=10 , b = 20 ;
+        int sum=a+b;
+        return sum;
+
+    }
+
+
+    public static void main(String[] args){
+        int a =  solve();
+        System.out.println(a);
+    }
+}
