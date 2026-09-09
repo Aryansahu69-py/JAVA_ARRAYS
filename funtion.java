04c679f9db1ea1df41c29843416f9c362237d2b4
Aryan sahu <lakhan9953@gmail.com>
2026-09-09 21:30:41 +0530

    Adding non Static Function

--- funtion.java
+++ funtion.java
@@ -1,17 +1,27 @@
 package JAVA_ARRAYS;
 
 public class funtion {
-    public static int solve(){
+    public static int solve(){ // int shows which type of value should be return
+        /*static is optional but if static is not given we have to make an object first 
+        then we can use it
+         
+*/
+
         System.out.println("Hello World");
         int a=10 , b = 20 ;
         int sum=a+b;
         return sum;
 
     }
+    public void print(){
+        System.out.println("Hello");
+    }
 
 
     public static void main(String[] args){
         int a =  solve();
         System.out.println(a);
+        funtion sc =new funtion(); // Declaring the non static Function
+        sc.print();
     }
 }
