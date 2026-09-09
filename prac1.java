e196befb50be5aad647bbe76dbdd7954f4a838f9
Aryan sahu <lakhan9953@gmail.com>
2026-09-09 22:12:18 +0530

    Adding Factroial Function

---
+++ prac1.java
@@ -0,0 +1,15 @@
+package JAVA_ARRAYS; //Finding the strong number but this using and Declaring our own function
+public class prac1 {
+    public static int fact(int num){
+        int temp=1;
+        for(int i=1;i<=num;i++){
+            temp *= i;
+            
+        }
+        return temp;
+    }
+    public static void main(String[] args){
+        
+    }
+    
+}
