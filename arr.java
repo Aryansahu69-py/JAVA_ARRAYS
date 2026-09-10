fe2ea8665dfaf6301819fe868ee3dce66857f618
Aryan sahu <lakhan9953@gmail.com>
2026-09-11 00:13:00 +0530

    learing the how  array function

--- arr.java
+++ arr.java
@@ -3,6 +3,13 @@ package JAVA_ARRAYS;
 public class arr {
     public static  void main(String[] args){
         int arr[]; //  this is array Declaration
+        int []arr1 = new int[]{1,2,3,4,5}; // in this case size  should not be given
+        int [] arr2 =  {1,2,3,4,5};
+        System.out.println(arr1); //This will give the memory location
+        // for printing the the elements we use loop
+        for(int i=0;  i<=(arr2.length -1); i++){
+            System.out.println(arr2[i]);
+        }
     }
     
 }
