f5cd06d26e1d7a558a8bfc94228becc404a92143
Aryan sahu <lakhan9953@gmail.com>
2026-09-13 19:26:52 +0530

    Adding some small optimization

--- prac11.java
+++ prac11.java
@@ -1,14 +1,17 @@
 package JAVA_ARRAYS;
-
+//left rotate by k elements
 import java.util.Arrays;
 
 public class prac11 {
     public static void main(String[] args) {
         int k = 4;
+
         int arr[] = { 1, 2, 3, 4, 5, 6 };
+        int n = arr.length;
+        k= k%n;
         for (int j = 1; j <= k; j++) {
             int temp = arr[0];
-            int n = arr.length;
+            
             for (int i = 1; i < n; i++) {
                 arr[i - 1] = arr[i];
             }
