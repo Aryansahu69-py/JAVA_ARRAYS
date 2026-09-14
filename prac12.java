038e903c751ecdbf50c199be1a121e3636874cbe
Aryan sahu <lakhan9953@gmail.com>
2026-09-14 20:21:59 +0530

    Adding user input

--- prac12.java
+++ prac12.java
@@ -1,12 +1,13 @@
 package JAVA_ARRAYS;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class prac12 {
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6};
         int n = arr.length;
-        int k = 3;
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
         
         for(int j = 1 ; j <= k ; j++){
             int temp =  arr[n-1];
