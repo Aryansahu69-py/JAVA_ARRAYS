e9c0db9ba9102a5fc3787c94b988ac2ed6da5558
Aryan sahu <lakhan9953@gmail.com>
2026-09-16 00:02:49 +0530

    completing the logic

--- prac14.java
+++ prac14.java
@@ -1,15 +1,18 @@
 package JAVA_ARRAYS;
-/*Learning lineary Search */
+/*Learning linear Search */
 public class prac14 {
-    int arr[] = {1,2,5,3,8,4,9};
-    int target = 8;
-    int ans = -1 ; 
-    for (int i = 0 ; i < arr.length ; i++){
-        if(arr[i] == target) {
-            ans = i;
-            break;
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 5, 3, 8, 4, 9};
+        int target = 8;
+        int ans = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                ans = i;
+                break;
+            }
         }
-        
+
+        System.out.println(ans);
     }
-    
 }
