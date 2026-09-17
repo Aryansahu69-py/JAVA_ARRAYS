f45f79615aa04222da4fef44795339fe725cf23c
Aryan sahu <lakhan9953@gmail.com>
2026-09-17 20:02:03 +0530

    writting the code for  bubble search by my own after learning the concept

--- prac16.java
+++ prac16.java
@@ -1,5 +1,23 @@
 package JAVA_ARRAYS;
 
+import java.util.Arrays;
+
+/*Sorting algo for unstable array ie bubble sort
+ */
 public class prac16 {
-    
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 5, 2, 8, 4 };
+        int n = arr.length;
+        for (int j = 0; j < n - 1; j++) {
+            for (int i = 0; i < n - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
