1ac8eb56c0c47e75e167a0bef81991a56daf1482
Aryan sahu <lakhan9953@gmail.com>
2026-09-17 20:07:10 +0530

    optimizing the code for fast sorting

--- prac16.java
+++ prac16.java
@@ -9,7 +9,7 @@ public class prac16 {
         int arr[] = { 6, 3, 5, 2, 8, 4 };
         int n = arr.length;
         for (int j = 0; j < n - 1; j++) {
-            for (int i = 0; i < n - 1; i++) {
+            for (int i = 0; i < n - 1 -i; i++) { //here n-1-i for reducing the steps as previous element is already sorted
                 if (arr[i] > arr[i + 1]) {
                     int temp = arr[i];
                     arr[i] = arr[i + 1];
