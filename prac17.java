97dc1feaf0a06ba2f258cd1c080aa9470786a7e3
Aryan sahu <lakhan9953@gmail.com>
2026-09-18 21:10:25 +0530

    Selection sort in which we are usig min value approach

--- prac17.java
+++ prac17.java
@@ -1,7 +1,24 @@
 package JAVA_ARRAYS;
+
+import java.util.Arrays;
+
 //Learning selection sort
 public class prac17 {
     public static void main(String[] args) {
+        int arr[] = {2,6,8,7,4,10,5};
+        int n = arr.length;
+        for(int i = 0 ; i<n-1 ; i++){
+            int min = i;
+            for(int j = i+1 ; j<n ; j++){
+                if (arr[j] < arr[min]){
+                    min = j ;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min]=temp;
+        }
+        System.out.println(Arrays.toString(arr));
         
     }
 }
