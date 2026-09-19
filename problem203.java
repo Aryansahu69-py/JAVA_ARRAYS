b4c8b5e1395fa318f42af199609c42aa347e9320
Aryan sahu <lakhan9953@gmail.com>
2026-09-19 20:32:09 +0530

    Read the comments inside the code

--- problem203.java
+++ problem203.java
@@ -1,5 +1,22 @@
 package JAVA_ARRAYS;
 
+import java.util.Arrays;
+
 public class problem203 {
-    
+    /*we have to shift the zero at the end of the array while maintaing the order*/ 
+    public static void main(String[] args) {
+        int arr[] = {1,2,0,5,9,0,0,4,7};
+        int n = arr.length;
+        int copy[] = new int[n];
+        int s = 0 , e = n-1;
+        for(int i = 0 ; i<n ; i++){
+            if(arr[i] != 0 ){
+                copy[s++] = arr[i];  
+            }else{
+                copy[e--] = arr[i];
+            }
+        }
+            arr = copy ; 
+        System.out.println(Arrays.toString(arr));
+    }
 }
